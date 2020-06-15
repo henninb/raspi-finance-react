09215d9d618a15bd4935ad0c3ee9b8a947363b39
Brian Henning <henninb@msn.com>
2020-06-15 18:50:39 -0500

    transaction updates

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,42 +1,28 @@
-import React, { Component } from "react";
+import React, {useState,useEffect} from "react";
 import MaterialTable from "material-table";
 import NavbarInstance from './NavbarInstance';
 import Spinner from './Spinner';
 import './master.scss';
 
-export default class TestTable extends Component {
-  state = {
-    loading: true,
-    data: [
-        { guid: 1, date: "6/12/2020", transaction: "test.com", category: "online", amount: 5.51, cleared: 1, notes: "none" },
+export default function TransactionTable () {
+
+  const timer = () => setTimeout(()=>{
+    setLoading(false)
+  }, 2300);
+
+  const [loading, setLoading] = useState(true);
+  useEffect(function() {timer()})
+
+   const [data, setData] = useState([
+     { guid: 1, date: "6/12/2020", transaction: "test.com", category: "online", amount: 5.51, cleared: 1, notes: "none" },
         { guid: 2, date: "6/13/2020", transaction: "Walgreens", category: "", amount: 10.51, cleared: 1, notes: "none" },
         { guid: 3, date: "4/3/2020", transaction: "aliexpress.com", category: "online", amount: 7.71, cleared: 1, notes: "none" },
         { guid: 4, date: "4/3/2020", transaction: "ebay.com", category: "online", amount: 4.43, cleared: 1, notes: "none" },
-      ]
-  }
+    ]);
 
-  componentDidMount() {
-    this.isLoading = setTimeout(()=>{this.setState({loading: false})}, 2300);
-  }
-
-  componentWillUnmount() {
-     clearTimeout(this.isLoading);
-  }
-
-  timer = () => setTimeout(()=>{
-    this.setState({loading: false})
-  }, 2300);
-
-  render() {
-    const {loading} = this.state;
-    const {data} = this.state;
-    let val;
-    if( loading )  {
-      //val = <div class="loading centered"></div>
-      val = <div class="centered"><Spinner /></div>
-    } else {
-    val =
-      <div class="table-formatting">
+  return (<div>
+        {!loading ?
+      <div className="table-formatting">
         <NavbarInstance />
         <MaterialTable
           columns={[
@@ -56,13 +42,41 @@ export default class TestTable extends Component {
             paging: false,
             search: true
           }}
+
+         editable={{
+           onRowAddCancelled: rowData => console.log("Row adding cancelled"),
+           onRowDeleteCancelled: rowData => console.log("Row deleting cancelled"),
+           onRowUpdateCancelled: rowData => console.log("Row editing cancelled"),
+           onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    setData([...data, newData]);
+                    resolve();
+                }, 1000);
+            }),
+           onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataUpdate = [...data];
+                  const index = oldData.tableData.id;
+                  dataUpdate[index] = newData;
+                  setData([...dataUpdate]);
+                  resolve();
+                }, 1000);
+            }),
+           onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const dataDelete = [...data];
+                    const index = oldData.tableData.id;
+                    dataDelete.splice(index, 1);
+                    setData([...dataDelete]);
+                    resolve();
+                }, 1000);
+             })
+         }}
         />
-      </div>;
-    }
-    return (
-      <div>
-        {val}
-      </div>
+      </div>: <div className="centered"><Spinner /></div>}</div>
     )
   }
-}
+
