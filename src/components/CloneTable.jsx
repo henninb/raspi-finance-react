457d55f0620456ad7724a143eb4b111c4a963cc1
Brian Henning <henninb@msn.com>
2020-08-30 10:11:33 -0500

    version updates

--- src/components/CloneTable.jsx
+++ src/components/CloneTable.jsx
@@ -1,5 +1,5 @@
 import MaterialTable from "material-table";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Spinner from "./Spinner";
 import React, {useState} from "react";
 
