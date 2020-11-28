a7f6d3242afb05b6e61b5ae6c8934c67692d2442
Brian Henning <henninb@msn.com>
2020-11-27 18:59:31 -0600

    docker setup

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@
 #FROM node:15.3.0-alpine
 FROM node:15.3.0
 
-# RUN curl -o- -L https://yarnpkg.com/install.sh | \  bash -s -- --version 0.26.1
-# RUN yarn global add nodemon@1.11.0
+RUN apt-get update -qq
+RUN apt-get install -y apt-transport-https ca-certificates
 
+RUN curl -k -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+RUN apt install -y yarn
 
 # set working directory
 WORKDIR /app
@@ -17,13 +21,16 @@ COPY package.json ./
 # COPY package-lock.json ./
 COPY yarn.lock ./
 # RUN npm install --silent
+RUN yarn config set "strict-ssl" false -g
 RUN yarn add react-scripts -g --silent
-RUN yarn build --profile production
+#RUN yarn build --profile production
 
 COPY .env ./
 
 # add app
 COPY . ./
 
+EXPOSE 3000
+
 # start app
-CMD ["yarn", "start", "--env=production"]
+CMD ["yarn", "start"]
--- run.sh
+++ run.sh
@@ -1,6 +1,47 @@
 #!/bin/sh
 
+# "$OSTYPE" == "darwin"*
+if [ "$OS" = "Linux Mint" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Raspbian GNU/Linux" ]; then
+  HOST_IP=$(hostname -I | awk '{print $1}')
+elif [ "$OS" = "Arch Linux" ]; then
+  HOST_IP=192.168.100.207
+elif [ "$OS" = "openSUSE Tumbleweed" ]; then
+  HOST_IP=192.168.100.193
+elif [ "$OS" = "Solus" ]; then
+  HOST_IP=192.168.100.118
+elif [ "$OS" = "Fedora" ]; then
+  HOST_IP=192.168.100.130
+elif [ "$OS" = "Darwin" ]; then
+  HOST_IP=$(ipconfig getifaddr en0)
+  # echo "lsof -nP | grep LISTEN"
+elif [ "$OS" = "void" ]; then
+  HOST_IP=127.0.0.1
+elif [ "$OS" = "Gentoo" ]; then
+  HOST_IP=$(hostname -i | awk '{print $1}')
+else
+  echo "$OS is not yet implemented."
+  exit 1
+fi
+
+export HOST_IP
+
+
+
 mkdir -p ssl
+
+
+    if ! docker-compose -f docker-compose.yml build; then
+      echo "docker-compose build failed."
+      exit 1
+    fi
+
+    if ! docker-compose -f docker-compose.yml up; then
+      echo "docker-compose up failed."
+      exit 1
+    fi
+
+
+
 echo yarn global add npm-check-updates
 ncu -u
 yarn install
