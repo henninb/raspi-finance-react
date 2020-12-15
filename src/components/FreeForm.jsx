3ee3ef35279b444522c51e70c024bceebc99d6b7
Brian Henning <henninb@msn.com>
2020-12-14 19:27:40 -0600

    updates to freeform

--- src/components/FreeForm.jsx
+++ src/components/FreeForm.jsx
@@ -16,19 +16,40 @@ export default function FreeForm() {
         setOpen(false);
     }
 
-    const handleCleanUp = () => {
+    const transformText = (text) => {
         const year = new Date().getFullYear().toString()
-        let text = document.getElementById("textArea").value
         text = text.replace(/[^\S\r\n]+$/gm, "")
         text = text.replace(/\nHOUSEHOLD/g, "\n")
         text = text.replace(/\nHousehold/g, "\n")
+        text = text.replace(/\nFood & drink\s+\$/g, ",")
+        text = text.replace(/\nShopping\s+\$/g, ",")
+        text = text.replace(/\nGroceries\s+\$/g, ",")
+        text = text.replace(/\nPersonal\s+\$/g, ",")
         text = text.replace(/CENTRAL CHECKOUT/g, "TARGET - ")
         text = text.replace(/ 800-591-3869\n\$/g, ",")
         text = text.replace(/\nTARGET.COM,/g, ",TARGET.COM,")
         text = text.replaceAll(" 800-591-3869", "")
+        text = text.replaceAll(" 00:00:00", "")
+        text = text.replaceAll("See details about this transaction", "")
+        text = text.replaceAll(" | ", ",")
         text = text.replaceAll(", " + year, " " + year)
+        //text = text.replace(/\s+2020\s+/g, " " + year + ",")
+        let re = new RegExp("\\s+" + year + "\\s+", "g")
+        text = text.replace(re, " " + year + ",")
         text = text.replace(/[\r\n]{2,}/g, "\n")
-        document.getElementById("textArea").value = text.trim()
+        text = text.replaceAll(", ", ",")
+        return text
+    }
+
+    const handlePaste = (e) => {
+        let text = e.clipboardData.getData('Text')
+        e.preventDefault();
+        document.getElementById("textArea").value = transformText(text).trim()
+    }
+    
+    const handleCleanUp = () => {
+        let text = document.getElementById("textArea").value
+        document.getElementById("textArea").value = transformText(text).trim()
         setMessage('data cleaned')
         setOpen(true)
     }
@@ -38,14 +59,14 @@ export default function FreeForm() {
         let prefix = document.getElementById("prefix").value.trim()
         let prefixedText = ""
 
-        if(prefix === '') {
+        if (prefix === '') {
             setMessage('fix empty prefix')
             setOpen(true)
             return
         }
 
         text.split(/\r?\n/).forEach((str) => {
-            prefixedText += prefix +"," + str + "\n"
+            prefixedText += prefix + "," + str.trim() + "\n"
         })
         document.getElementById("textArea").value = prefixedText.trim()
         document.getElementById("prefix").value = ""
@@ -56,8 +77,8 @@ export default function FreeForm() {
     const validateData = () => {
         let text = document.getElementById("textArea").value.trim()
         text.split(/\r?\n/).forEach((str) => {
-            if (str.split(',').length !== 5 ) {
-                setMessage("invalid record='" + str + "'")
+            if (str.split(',').length !== 4) {
+                setMessage("invalid record count= " + str.split(',').length + " ='" + str + "'")
                 setOpen(true)
             }
         })
@@ -65,7 +86,7 @@ export default function FreeForm() {
         //setOpen(true)
     }
 
-    const handleError = (error, moduleName, throwIt) =>  {
+    const handleError = (error, moduleName, throwIt) => {
         if (error.response) {
             setMessage(`${moduleName}: ${error.response.status} and ${JSON.stringify(error.response.data)}`)
             console.log(`${moduleName}: ${error.response.status} and ${JSON.stringify(error.response.data)}`)
@@ -107,7 +128,7 @@ export default function FreeForm() {
         const lines = sanitizedText.split(os.EOL)
 
         for (const line of lines) {
-            const columns = line.split(',')
+            const columns = line.trim().split(',')
             let accountNameOwner = columns[0]
             let transactionDate = columns[1]
             let description = columns[2]
@@ -154,24 +175,31 @@ export default function FreeForm() {
             }
 
         }
-        setMessage('all records are submitted.')
-        setOpen(true)
+        // setMessage('all records are submitted.')
+        // setOpen(true)
     }
 
-    //onPaste={() => handlePaste()}
+    //select 'centerpoint_brian', (transaction_date + interval '1 year'), 'centerpoint energy', abs(amount) from t_transaction
+    // where description like '%centerpoint%' and extract(year from transaction_date) = 2020
+
+    //select 'xcel-energy_brian', (transaction_date + interval '1 year'), 'centerpoint energy', abs(amount) from t_transaction
+    // where description like '%xcel%' and extract(year from transaction_date) = 2020
+
+    //
     return (
         <div className="freeform">
 
             <div>
-                <textarea name="comment" form="transactions" id="textArea" rows="20" cols="180" defaultValue=""  />
+                <textarea name="comment" form="transactions" id="textArea" rows="20" cols="180" defaultValue=""
+                          onPaste={(event) => handlePaste(event)}/>
                 <p>
-                    <input type="text" id="prefix" />
+                    <input type="text" id="prefix"/>
                 </p>
                 <p>
-                <input type="button" value="clean" onClick={() => handleCleanUp()}/>
-                <input type="button" value="prefix" onClick={() => handlePrefix()}/>
-                <input type="button" value="validate" onClick={() => validateData()}/>
-                <input type="submit" value="submit" onClick={() => handleChange()}/>
+                    <input type="button" value="clean" onClick={() => handleCleanUp()}/>
+                    <input type="button" value="prefix" onClick={() => handlePrefix()}/>
+                    <input type="button" value="validate" onClick={() => validateData()}/>
+                    <input type="submit" value="submit" onClick={() => handleChange()}/>
                 </p>
             </div>
 
