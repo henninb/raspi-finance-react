7d34213719c57550ab1575b43c992cde52a76b82
Brian Henning <henninb@msn.com>
2021-02-27 07:00:53 -0600

    fixed update date bug

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -279,7 +279,6 @@ export default function TransactionTable() {
         let endpoint = endpointUrl() + "/transaction/update/" + oldData.guid
         delete newData["tableData"]
 
-        //newData["transactionDate"] = formatDate(newData.transactionDate)
         if (newData.receiptImage !== undefined) {
             newData['receiptImage'].image = newData['receiptImage'].image.replace(/^data:image\/[a-z]+;base64,/, "")
         }
