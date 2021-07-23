073623e77cbc1964fc899b5fb4cc848be6b42c70
Brian Henning <henninb@msn.com>
2021-07-23 05:55:59 -0500

    updates

--- src/components/queries/useGraphqlFetchDescription.tsx
+++ src/components/queries/useGraphqlFetchDescription.tsx
@@ -1,16 +1,29 @@
 import axios, { AxiosError } from "axios";
 import { endpointUrl } from "../Common";
-import { useQuery } from "react-query";
-import gql from "graphql-tag";
+//import { useQuery } from "react-query";
+// import gql from "graphql-tag";
 
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { Query } from "react-apollo";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: endpointUrl() + "/graphql",
+    cache: new InMemoryCache()
+});
+
+// import { ApolloProvider } from "react-apollo";
+// import { ApolloClient } from "apollo-client";
+// import { Query } from "react-apollo";
 
 //import { HttpLink } from 'apollo-link-http';
 //import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const query1 = gql`
+const query = gql`
   query {
     descriptions {
       description
@@ -19,44 +32,14 @@ const query1 = gql`
 `;
 
 const fetchDescriptionData = async (): Promise<any> => {
-  // <Query query={query1} >
-  //   {/*{({ data: { organization }, loading }) => {*/}
-  //   {/*  if (loading || !organization) {*/}
-  //   {/*    return <div>Loading ...</div>;*/}
-  //   {/*  }*/}
-  //
-  //   {/*  return (*/}
-  //   {/*      <RepositoryList repositories={organization.repositories} />*/}
-  //   {/*  );*/}
-  //   {/*}}*/}
-  //   return (<div>test</div>
-  //   )
-  // </Query>
+    let response = client.query({
+        query: query
+    })
+    //.then(result => console.log(result));
+    return response
+  };
 
-  const query = '{"query":"query{descriptions {  description }}"}';
-  const response = await axios.post(endpointUrl() + "/graphql", query, {
-    timeout: 0,
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-
-  return response.data;
-};
 
 export default function useGraphqlFetchDescription() {
-  return useQuery("description", () => fetchDescriptionData(), {
-    onError: (error: AxiosError<any>) => {
-      console.log(error ? error : "error is undefined.");
-      console.log(
-        error.response ? error.response : "error.response is undefined."
-      );
-      console.log(
-        error.response
-          ? JSON.stringify(error.response)
-          : "error.response is undefined - cannot stringify."
-      );
-    },
-  });
+  return useQuery(query);
 }
