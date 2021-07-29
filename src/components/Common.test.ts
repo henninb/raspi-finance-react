ca54c1173e6ce787adc43dc5562edf8e20fe301c
Brian Henning <henninb@msn.com>
2021-07-29 18:06:53 -0500

    added jest tests

--- src/components/Common.test.ts
+++ src/components/Common.test.ts
@@ -1,21 +1,33 @@
-import { isFloat, capitalizeFirstChar } from "./Common";
+import { isFloat, capitalizeFirstChar, endpointUrl, epochToDate } from "./Common";
 
 test("properly determines if this is a float - should be false", () => {
   expect(isFloat(10)).toBeFalsy()
 });
 
-test("properly determines if this is a float - shoud be true", () => {
+test("properly determines if this is a float - should be true", () => {
     expect(isFloat(10.1)).toBeTruthy();
 });
 
 test('determine capitalize first letter', () => {
-    expect(capitalizeFirstChar('foo')).toBe('Foo')
+    expect(capitalizeFirstChar('foo')).toStrictEqual('Foo')
 })
 
 test('determine capitalize first letter - two words', () => {
-    expect(capitalizeFirstChar('foo bar')).toBe('Foo bar')
+    expect(capitalizeFirstChar('foo bar')).toStrictEqual('Foo bar')
 })
 
 test('determine capitalize first letter - already capital', () => {
-    expect(capitalizeFirstChar('Foo bar')).toBe('Foo bar')
+    expect(capitalizeFirstChar('Foo bar')).toStrictEqual('Foo bar')
+})
+
+test('determine endpoint URL', () => {
+    expect(endpointUrl()).toMatch(/(http|https)/)
+    expect(endpointUrl()).toContain(":" + process.env.REACT_APP_ENDPOINT_PORT)
+    expect(endpointUrl()).toContain( process.env.REACT_APP_ENDPOINT_SERVER + ":")
+
+})
+
+test('determine the epoch date', () => {
+    console.log(new Date('1970-01-01T00:00:00.000Z'))
+    expect(epochToDate(0)).toBe(new Date('1970-01-01T00:00:00.000Z'))
 })
