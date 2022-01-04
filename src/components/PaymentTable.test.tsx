45ec2d0a163547def14f61766e48c2d25af036cb
Brian Henning <henninb@msn.com>
2022-01-04 08:36:40 -0600

    wip

--- src/components/PaymentTable.test.tsx
+++ src/components/PaymentTable.test.tsx
@@ -3,8 +3,8 @@ import { act } from "@testing-library/react";
 import { render } from "./test-utils";
 import PaymentTable from "./PaymentTable";
 import { fireEvent } from "@testing-library/dom";
-import { MockedProvider } from '@apollo/react-testing';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { MockedProvider } from "@apollo/react-testing";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 const MOCKS = [];
 
@@ -19,12 +19,12 @@ describe("paymentTableTests", () => {
   beforeEach(async () => {
     jest.clearAllMocks();
     await act(() => {
-    const queryClient = new QueryClient()
+      const queryClient = new QueryClient();
 
       wrapper = render(
-      <QueryClientProvider client={queryClient}>
-            <PaymentTable />
-            </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <PaymentTable />
+        </QueryClientProvider>
       );
     });
   });
@@ -33,26 +33,25 @@ describe("paymentTableTests", () => {
     expect(true).toBeTruthy();
   });
 
-
-//   it("paymentTable - add a record", () => {
-//     //console.log(wrapper);
-//     const { getByLabelText, getByTitle, getByPlaceholderText } = wrapper;
-//     // eslint-disable-next-line
-//     let paymentTable = getByLabelText("payment-table");
-//
-//     //TODO: set the hook value of loading to true otherwise this will not work
-//     let addButton = getByTitle("Add");
-//
-//     //const {getByLabelText} = render(<PaymentTable/>);
-//     console.log(getByLabelText("payment-table"));
-//
-//     fireEvent.click(addButton);
-//     //let transactionDate = getByPlaceholderText("transactionDate");
-//     let amount = getByPlaceholderText("amount");
-//
-//     fireEvent.change(amount, { target: { value: 10 } });
-//     //fireEvent.accountNameOwner(amount, {target: {value: 'test_brian'}});
-//
-//     expect(amount.value).toBe("10");
-//   });
+  //   it("paymentTable - add a record", () => {
+  //     //console.log(wrapper);
+  //     const { getByLabelText, getByTitle, getByPlaceholderText } = wrapper;
+  //     // eslint-disable-next-line
+  //     let paymentTable = getByLabelText("payment-table");
+  //
+  //     //TODO: set the hook value of loading to true otherwise this will not work
+  //     let addButton = getByTitle("Add");
+  //
+  //     //const {getByLabelText} = render(<PaymentTable/>);
+  //     console.log(getByLabelText("payment-table"));
+  //
+  //     fireEvent.click(addButton);
+  //     //let transactionDate = getByPlaceholderText("transactionDate");
+  //     let amount = getByPlaceholderText("amount");
+  //
+  //     fireEvent.change(amount, { target: { value: 10 } });
+  //     //fireEvent.accountNameOwner(amount, {target: {value: 'test_brian'}});
+  //
+  //     expect(amount.value).toBe("10");
+  //   });
 });
--- src/components/test-utils.tsx
+++ src/components/test-utils.tsx
@@ -2,9 +2,9 @@ import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
 // create a customRender that wraps the UI in a memory Router
-const customRender = (ui, options) => {
+const customRender = (ui: any, options: any) => {
   return render(ui, { wrapper: MemoryRouter, ...options });
-}
+};
 
 // re-export everything
 export * from "@testing-library/react";
