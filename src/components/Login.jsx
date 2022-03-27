add66f35103c917da13cad80346a31c88d30b0a8
Brian Henning <henninb@msn.com>
2022-03-27 14:59:18 -0500

    wip

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,63 +1,59 @@
 import { useEffect } from "react";
-//import axios from "axios";
 
 export default function Login(props) {
-  // export const Login = (props) => {
 
   const handleClick = (e) => {
     console.log("login submit was clicked");
-    document.getElementById('login-result').innerHTML='comment';
+    document.getElementById("login-result").innerHTML = "comment";
   };
-
+  
   useEffect(() => {
     // Update the document title using the browser API
     // document.title = `You clicked ${count} times`;
   });
 
   return (
-    <div className="login centered">
-
-      <div id="login-result">none</div>
-
-      <div className="form">
-        <form
-          name="login-form"
-          className="login-form"
-          action="/login"
-          method="GET"
-          data-bitwarden-watching="1"
-        >
-          <span className="material-icons">lock</span>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
+      <div className="login centered">
+        <div className="form">
+          <form
+              name="login-form"
+              className="login-form"
+              action="/login"
+              method="GET"
+              data-bitwarden-watching="1"
+          >
+            <span className="material-icons">lock</span>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
               <span className="input-group-text">
                 <i className="fa fa-user form-icon" />
               </span>
+              </div>
+              <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                  id="email"
+                  name="email"
+              />
             </div>
-            <input
-                type="text"
-                className="form-control"
-                placeholder="Email"
-                id="email"
-                name="email"
-            />
-
-            <div className="input-group-prepend">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
               <span className="input-group-text">
                 <i className="fa fa-lock form-icon" />
               </span>
+              </div>
+              <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  id="password"
+                  name="password"
+              />
             </div>
-            <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                id="password"
-                name="password"
-            />
-          </div>
-          <button type="submit" onClick={handleClick}>login</button>
-        </form>
+            <button type="submit" onClick={handleClick}>login</button>
+          </form>
+        </div>
       </div>
-    </div>
   );
 }
--- src/components/NavbarInstance.tsx
+++ src/components/NavbarInstance.tsx
@@ -5,44 +5,94 @@ import "./main.scss";
 
 export default function NavbarInstance() {
   return (
-    <div className="white-div bg-primary">
-      <nav className="navbar navbar-expand-sm nav-purple navbar-light fixed-top font-weight-bold">
-        <Link className="navbar-brand" to="/">
-          FinanceApp
-        </Link>
+    <div>
+      {/*<nav className="navbar navbar-expand-sm nav-purple navbar-light fixed-top font-weight-bold">*/}
+      {/*  <Link className="navbar-brand" to="/">*/}
+      {/*    FinanceApp*/}
+      {/*  </Link>*/}
 
-        <ul className="navbar-nav">
-          <li className="nav-item font-weight-bold">
-            <Link
-              className="nav-link"
-              to="/payments"
-              data-test-id="payments-link"
-            >
-              Payments
-            </Link>
-          </li>
-          <li className="nav-item font-weight-bold">
-            <Link
-              className="nav-link"
-              to="/payment/required"
-              data-test-id="payment-required-link"
-            >
-              PaymentRequired
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              to="/freeform"
-              data-test-id="freeform-link"
-            >
-              FreeForm
-            </Link>
-          </li>
-          <li>
-            <SelectAccounts />
-          </li>
-        </ul>
+      {/*  <ul className="navbar-nav">*/}
+      {/*    <li className="nav-item font-weight-bold">*/}
+      {/*      <Link*/}
+      {/*        className="nav-link"*/}
+      {/*        to="/payments"*/}
+      {/*        data-test-id="payments-link"*/}
+      {/*      >*/}
+      {/*        Payments*/}
+      {/*      </Link>*/}
+      {/*    </li>*/}
+      {/*    <li className="nav-item font-weight-bold">*/}
+      {/*      <Link*/}
+      {/*        className="nav-link"*/}
+      {/*        to="/payment/required"*/}
+      {/*        data-test-id="payment-required-link"*/}
+      {/*      >*/}
+      {/*        PaymentRequired*/}
+      {/*      </Link>*/}
+      {/*    </li>*/}
+      {/*    <li className="nav-item">*/}
+      {/*      <Link*/}
+      {/*        className="nav-link"*/}
+      {/*        to="/freeform"*/}
+      {/*        data-test-id="freeform-link"*/}
+      {/*      >*/}
+      {/*        FreeForm*/}
+      {/*      </Link>*/}
+      {/*    </li>*/}
+      {/*    <li>*/}
+      {/*      <SelectAccounts />*/}
+      {/*    </li>*/}
+      {/*  </ul>*/}
+      {/*</nav>*/}
+
+      {/*<nav className="navbar navbar-expand-sm nav-purple navbar-light fixed-top font-weight-bold">*/}
+      <nav className="navbar navbar-expand-lg navbar-light navbar-fixed-top">
+        {/*<a className="navbar-brand" href="#">*/}
+        {/*  Navbar*/}
+        {/*</a>*/}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              {" "}
+              <a className="nav-link" href="/">
+                Home<span className="sr-only">(current)</span>
+              </a>{" "}
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/payments">
+                Payments
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/payment/required">
+                Payment Required
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/freeform">
+                FreeForm
+              </a>
+            </li>
+            <li className="nav-item">
+              {" "}
+              <a className="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+        <SelectAccounts />
       </nav>
     </div>
   );
