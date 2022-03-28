c7a3af454c07985144e0b08690eb9e99a9d64db0
Brian Henning <henninb@msn.com>
2022-03-28 07:13:39 -0500

    spinner

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 
 export default function Login(props) {
-
   const handleClick = (e) => {
     console.log("login submit was clicked");
     document.getElementById("login-result").innerHTML = "comment";
@@ -13,47 +12,49 @@ export default function Login(props) {
   });
 
   return (
-      <div className="login centered">
-        <div className="form">
-          <form
-              name="login-form"
-              className="login-form"
-              action="/login"
-              method="GET"
-              data-bitwarden-watching="1"
-          >
-            <span className="material-icons">lock</span>
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
+    <div className="login centered">
+      <div className="form">
+        <form
+          name="login-form"
+          className="login-form"
+          action="/login"
+          method="GET"
+          data-bitwarden-watching="1"
+        >
+          <span className="material-icons">lock</span>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
               <span className="input-group-text">
                 <i className="fa fa-user form-icon" />
               </span>
-              </div>
-              <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Email"
-                  id="email"
-                  name="email"
-              />
             </div>
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              id="email"
+              name="email"
+            />
+          </div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
               <span className="input-group-text">
                 <i className="fa fa-lock form-icon" />
               </span>
-              </div>
-              <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                  id="password"
-                  name="password"
-              />
             </div>
-            <button type="submit" onClick={handleClick}>login</button>
-          </form>
-        </div>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              id="password"
+              name="password"
+            />
+          </div>
+          <button type="submit" onClick={handleClick}>
+            login
+          </button>
+        </form>
       </div>
+    </div>
   );
 }
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Loader from "react-loader-spinner";
+// import Loader from "react-loader-spinner";
 
 export default function Spinner() {
   return (
-    <div data-testid="loader">
-      <Loader type="Rings" color="#9965f4" />
+    <div>
+      <div data-testid="loader">
+        {/*<Loader type="Rings" color="#9965f4" />*/}
+
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
