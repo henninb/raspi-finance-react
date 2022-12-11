736a632f36e86d967b354c1aacadfcfcca8ced84
Brian Henning <henninb@msn.com>
2022-12-11 10:23:57 -0600

    wip

--- src/components/queries/useFetchCategory.tsx
+++ src/components/queries/useFetchCategory.tsx
@@ -1,7 +1,9 @@
+import axios, { AxiosError } from "axios";
 import { useQuery } from "react-query";
 import { gql } from "@apollo/client";
 import { ApolloError } from "apollo-client";
 import { apolloClient } from "../ApolloClient";
+import { basicAuth, endpointUrl } from "../Common";
 
 const query = gql`
   query {
@@ -11,18 +13,47 @@ const query = gql`
   }
 `;
 
-const fetchCategoryData = async () => {
-  const client = apolloClient();
-  const { data } = await client.query({ query });
-  console.log("fetching categories");
-  return data.categories;
+// const graphqlCategoryData = async () => {
+//   const client = apolloClient();
+//   const { data } = await client.query({ query });
+//   console.log("fetching categories");
+//   return data.categories;
+// };
+
+const fetchCategoryData = async (): Promise<any> => {
+  const response = await axios.get(endpointUrl() + "/category/select/active", {
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: basicAuth(),
+    },
+  });
+  //console.debug(JSON.stringify(response.data));
+  return response.data;
 };
+
+// export default function useFetchCategory() {
+//   return useQuery("category", () => fetchCategoryData(), {
+//     onError: (error: ApolloError) => {
+//       console.log("graphql error with category");
+//       //console.log(error ? error : "error is undefined.");
+//     },
+//   });
+// }
 
 export default function useFetchCategory() {
   return useQuery("category", () => fetchCategoryData(), {
-    onError: (error: ApolloError) => {
-      console.log("graphql error with category");
-      //console.log(error ? error : "error is undefined.");
+    onError: (error: AxiosError<any>) => {
+      console.log(error ? error : "error is undefined.");
+      console.log(
+        error.response ? error.response : "error.response is undefined."
+      );
+      console.log(
+        error.response
+          ? JSON.stringify(error.response)
+          : "error.response is undefined - cannot stringify."
+      );
     },
   });
 }
--- src/components/queries/useFetchDescription.tsx
+++ src/components/queries/useFetchDescription.tsx
@@ -1,8 +1,9 @@
-//import { client } from "../Common";
+import axios, { AxiosError } from "axios";
 import { useQuery } from "react-query";
 import { gql } from "@apollo/client";
 import { ApolloError } from "apollo-client";
 import { apolloClient } from "../ApolloClient";
+import { basicAuth, endpointUrl } from "../Common";
 
 const query = gql`
   query {
@@ -12,18 +13,47 @@ const query = gql`
   }
 `;
 
-const fetchDescriptionData = async () => {
-  const client = apolloClient();
-  const { data } = await client.query({ query });
-  console.log("fetching descriptions");
-  return data.descriptions;
+// const graphqlDescriptionData = async () => {
+//   const client = apolloClient();
+//   const { data } = await client.query({ query });
+//   console.log("fetching descriptions");
+//   return data.descriptions;
+// };
+
+const fetchDescriptionData = async (): Promise<any> => {
+  const response = await axios.get(endpointUrl() + "/description/select/all", {
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: basicAuth(),
+    },
+  });
+  //console.debug(JSON.stringify(response.data));
+  return response.data;
 };
+
+// export default function useFetchDescription() {
+//   return useQuery("description", () => fetchDescriptionData(), {
+//     onError: (error: ApolloError) => {
+//       console.log("graphql error with description");
+//       //console.log(error ? error : "error is undefined.");
+//     },
+//   });
+// }
 
 export default function useFetchDescription() {
   return useQuery("description", () => fetchDescriptionData(), {
-    onError: (error: ApolloError) => {
-      console.log("graphql error with description");
-      //console.log(error ? error : "error is undefined.");
+    onError: (error: AxiosError<any>) => {
+      console.log(error ? error : "error is undefined.");
+      console.log(
+        error.response ? error.response : "error.response is undefined."
+      );
+      console.log(
+        error.response
+          ? JSON.stringify(error.response)
+          : "error.response is undefined - cannot stringify."
+      );
     },
   });
 }
