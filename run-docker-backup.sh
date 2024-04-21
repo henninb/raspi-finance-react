15331c11789b3735932641f73b851b90f3b25575
Brian Henning <henninb@msn.com>
2024-04-21 08:34:39 -0500

    wip

--- run-docker-backup.sh
+++ run-docker-backup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 date=$(date '+%Y-%m-%d')
 
+export DOCKER_HOST=ssh://192.168.10.10
+docker images
 docker save -o raspi-finance-react-${date}.tar raspi-finance-react
 
+cp raspi-finance-react-${date}.tar /mnt/external/proxmox-backups/
+rm raspi-finance-react-${date}.tar
+
 echo docker load -i raspi-finance-react-${date}.tar
-
-scp raspi-finance-react-${date}.tar henninb@192.168.10.10:/home/henninb
-
 echo docker run -dit --restart unless-stopped -p 3000:443 --name raspi-finance-react -h raspi-finance-react raspi-finance-react
 
 exit 0
